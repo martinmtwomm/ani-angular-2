3f5ea1e152f2ccd893edfcba6fe01802ec41d264
martinm <martinmtwo@gmail.com>
2017-11-13 14:42:35 +0100

    -modified source code. fix loading problem due to case sensitive filename -modified source code to optain a cleaner view -added simple example to display image from assets in page -several changes to dashboard (avatar removal, change of names, removed example menu entries)

--- src/client/app/dashboard/bs-component/bsComponent.component.ts
+++ src/client/app/dashboard/bs-component/bsComponent.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
 	moduleId: module.id,
     selector: 'bs-component',
-    templateUrl: './bs-component.component.html'
+    templateUrl: './bs-Component.component.html'
 })
 
 export class BSComponentComponent {}
